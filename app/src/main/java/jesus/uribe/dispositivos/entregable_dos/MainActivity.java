d3741b8bbe12db3354994dd62732397b60587356
JesusUribe3 <chucho.jo33@gmail.com>
2023-04-18 02:15:50 -0500

    Finalizacion entrega_dos
    Se realiza la entrega dos, funcionando todo ok.

--- app/src/main/java/jesus/uribe/dispositivos/entregable_dos/MainActivity.java
+++ app/src/main/java/jesus/uribe/dispositivos/entregable_dos/MainActivity.java
@@ -3,18 +3,63 @@ package jesus.uribe.dispositivos.entregable_dos;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.content.Intent;
-import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
-    ListView Lista;
+    ListView lista;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        int [] idImagen = {
+                R.drawable.ronaldinho,
+                R.drawable.one_piece,
+                R.drawable.gmail,
+                R.drawable.mustang,
+                R.drawable.barca,
+                R.drawable.instagram,
+
+        };
+        String[] nombres = {
+                "Ronaldo", "One_Piece", "Gmail", "Mustang", "Barca", "Instagram"
+        };
+        String[] title ={
+                "Nuevo mensaje de Ronaldinho", "Anime", "Nuevo inicio de sesión", "autos", "Club de Futbol", "Tu publicación es correcta"
+        };
+        String[] mensajes = {
+                "Hola, por favor enviar la información...", "Tienes un nuevo capitulo disponible", "Intentaste acceder a tu cuenta...", "Adquiere este vehículo clásico",  "El club fue fundado oficialmente el 29 de noviembre de 1899", "Nuevo mensaje de usuario..."
+        };
+
+        String[] horaUltimoMensaje ={
+                "6:25", "13:15", "00:36", "03:35", "01:15", "23:23"
+        };
+        String[] numeroTelefon ={
+                "3101010", "3112178456", "3207453231", "3045214987", "3132178485", "31112145246"
+        };
+        String[] ciudad = {
+                "Medellin", "Barcelona", "Tokyo", "Berlin", "Olmo", "New York"
+        };
+        ListAdapter ListAdapter = new ListAdapter(MainActivity.this, nombres, title, mensajes,  horaUltimoMensaje, numeroTelefon,ciudad, idImagen);
+        lista = (ListView) findViewById(R.id.listaView);
+        lista.setAdapter(ListAdapter);
+        lista.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent irAinformacion = new Intent(MainActivity.this, Recibir.class)
+                        .putExtra("nombre", nombres[position])
+                        .putExtra("titulo", title[position])
+                        .putExtra("mensaje", mensajes[position])
+                        .putExtra("hora", horaUltimoMensaje[position])
+                        .putExtra("telefono", numeroTelefon[position])
+                        .putExtra("ciudad", ciudad[position])
+                        .putExtra("imagen", idImagen[position]);
+
+                startActivity(irAinformacion);
+            }
+        });
     }
 }
--- app/src/main/java/jesus/uribe/dispositivos/entregable_dos/Recibir.java
+++ app/src/main/java/jesus/uribe/dispositivos/entregable_dos/Recibir.java
@@ -1,13 +1,51 @@
 package jesus.uribe.dispositivos.entregable_dos;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
 
 public class Recibir extends AppCompatActivity {
 
+    String nombre, titulo, mensaje, hora, telefono, ciudad;
+
+    TextView textoNombre, textoTitulo, textoMensaje, textoHora, textoTelefono, textoCiudad;
+
+    int imagen;
+
+    CircleImageView image;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recibir);
+        textoNombre = findViewById(R.id.textoNombre);
+        textoTitulo = findViewById(R.id.textoTitulo);
+        textoMensaje = findViewById(R.id.textoMensaje);
+        textoTelefono = findViewById(R.id.telefono);
+        textoCiudad = findViewById(R.id.ciudad);
+        textoHora = findViewById(R.id.tv_ultimaHora);
+        image = findViewById(R.id.imagen);
+
+        Intent datos = getIntent();
+        if (datos !=null){
+            nombre = datos.getStringExtra("nombre");
+            titulo = datos.getStringExtra("titulo");
+            mensaje = datos.getStringExtra("mensaje");
+            hora = datos.getStringExtra("hora");
+            telefono = datos.getStringExtra("telefono");
+            ciudad = datos.getStringExtra("ciudad");
+            imagen = datos.getIntExtra("imagen", R.drawable.ronaldinho);
+
+            textoCiudad.setText(ciudad);
+            textoNombre.setText(nombre);
+            textoTelefono.setText(telefono);
+            textoTitulo.setText((titulo));
+            textoMensaje.setText((mensaje));
+            image.setImageResource(imagen);
+        }
     }
 }
